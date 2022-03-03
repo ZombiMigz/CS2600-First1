5b41de425ed39ae591766082819e32a96a9055f6
Luciano Lim <lmlim@cpp.edu>
2022-03-02 21:27:29 -0800

    adds outline to NumberGuessingGame

---
+++ NumberGuessingGame.c
@@ -0,0 +1,13 @@
+/*
+OUTLINE
+1) create max number variable
+2) create function to generate random number based on max number
+3) create function to play game (ask user and return correct or wrong)
+4) create struct for game results
+5) add store game results in game function
+6) add q(quit) option in game function
+7) create setMaxValue function (print max settable value and read input)
+8) add error checking on setMaxValue function
+9) create main function with while loop checking inputs
+10) add option 3 to inputs (thanks user, prints results, return 0)
+*/
