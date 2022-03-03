9ea6afd4184a80a9d3d8fef463c5d44838b08d6b
Luciano Lim <lmlim@cpp.edu>
2022-03-02 21:22:45 -0800

    removes instructions from RestaurantBill

--- RestaurantBill.c
+++ RestaurantBill.c
@@ -1,16 +1,5 @@
 /*
 OUTLINE
-Restaurant Bill
-Write a program that computes the tax and tip on a restaurant bill for a patron. The
-program should accept the tax and tip both as percentages from the command line when
-running the program. Meaning as argument passed into the main(). Display the meal cost,
-tax amount, tip amount, and total bill on the screen. The meal cost should be randomly
-chosen between the following four:
-Salad: $9.95
-Soup: $4.55
-Sandwich: $13.25
-Pizza: $22.35
-
 1) create struct for FoodItem
 2) create array of FoodItems
 3) randomly pick from array
