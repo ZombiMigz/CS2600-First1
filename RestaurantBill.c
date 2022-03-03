d99349cea8f56c5481904ed12bb4074c675b2275
Luciano Lim <lmlim@cpp.edu>
2022-03-02 20:38:48 -0800

    creates outline for Restaurant Bill program

---
+++ RestaurantBill.c
@@ -0,0 +1,20 @@
+/*
+Restaurant Bill
+Write a program that computes the tax and tip on a restaurant bill for a patron. The
+program should accept the tax and tip both as percentages from the command line when
+running the program. Meaning as argument passed into the main(). Display the meal cost,
+tax amount, tip amount, and total bill on the screen. The meal cost should be randomly
+chosen between the following four:
+Salad: $9.95
+Soup: $4.55
+Sandwich: $13.25
+Pizza: $22.35
+
+1) create struct for FoodItem
+2) create array of FoodItems
+3) randomly pick from array
+4) get user input for tax and tip (as percentages) using scanf
+5) calculate costs
+6) print out costs
+
+*/
